12cc2023400d30dda031aacad6b5dd8636fa2851
Felix Heidecke <felix@heidecke.me>
2017-08-23 12:06:06 +0200

    Improve setup check; Remove "compress" task reference

--- gruntfile.js
+++ gruntfile.js
@@ -188,7 +188,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-compress');
 
     grunt.registerMultiTask('cdnjs', 'Fetch JSON from CDN', function() {
 
--- setup.bat
+++ setup.bat
@@ -5,6 +5,18 @@ echo * * * * * * * * * * * * * * * * *
 echo * KR-Boilerplate konfigurieren! *
 echo * * * * * * * * * * * * * * * * *
 echo.
-pause
+echo Wurde die grunt.js entsprechend dem Projekt angepasst? (j/n)
+SET /p wahl=
+if '%wahl%' == 'n' goto Nein
+if '%wahl%' == 'j' goto Ja
+Goto Ende
+:Nein
+goto Ende
+:Ja
 call npm install
 call npm run init
+:Ende
+echo.
+echo Setup Prozess wurde beendet.
+echo.
+pause
