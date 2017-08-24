cca38e1ccd6824535b9ca81b80c91c1dd9ee7ffb
Felix Heidecke <felix@heidecke.me>
2017-08-24 23:47:19 +0200

    Imporve handling of vendor files

--- gruntfile.js
+++ gruntfile.js
@@ -1,84 +1,78 @@
 let vendor = {
 
-    // --- http://jquery.com/ --------------------------------------------------
+    'jquery' : { // ------------------------------------------------- jQuery ---
+        install : true,
+        js : [
+            'jquery.min.js'
+        ]
+    },
 
-    'jquery' : [
-        'jquery.min.js'
-    ],
+    'pure' : { // ------------------------------------------------- pure CSS ---
+        install : true,
+        css: [
+            'base-min.css',
+            'grids-min.css',
+            'grids-responsive-min.css'
+        ]
+    },
 
-    // --- http://purecss.io/ --------------------------------------------------
+    'font-awesome' : { // ------------------------------------- Font Awesome ---
+        install : true,
+        css: [
+            'css/font-awesome.min.css'
+        ],
+        fonts: [
+            'fonts/fontawesome-webfont.eot',
+            'fonts/fontawesome-webfont.svg',
+            'fonts/fontawesome-webfont.ttf',
+            'fonts/fontawesome-webfont.woff',
+            'fonts/fontawesome-webfont.woff2'
+        ]
+    },
 
-    'pure' : [
-        'base-min.css',
-        'grids-min.css',
-        'grids-responsive-min.css'
-    ],
+   'vue' : { // ----------------------------------------------------- Vue.js ---
+        install : false,
+        js : [
+           'vue.min.js'
+        ]
+   },
 
-    // --- http://fontawesome.io/ ----------------------------------------------
+    'mobile-detect' : { // ----------------------------------- Mobile Detect ---
+        install: false,
+        js: [
+            'mobile-detect.min.js'
+        ]
+    },
 
-    'font-awesome' : [
-        'css/font-awesome.min.css',
-        'fonts/fontawesome-webfont.eot',
-        'fonts/fontawesome-webfont.svg',
-        'fonts/fontawesome-webfont.ttf',
-        'fonts/fontawesome-webfont.woff',
-        'fonts/fontawesome-webfont.woff2'
-    ],
+    'simplelightbox' : { // -------------------------------- Simple lightbox ---
+        install: false,
+        js: [
+            'simple-lightbox.min.js'
+        ],
+        css: [
+            'simplelightbox.min.css'
+        ]
+    },
 
-    // --- http://vuejs.org/ ---------------------------------------------------
-
-/*
-  'vue' : [
-        'vue.min.js'
-    ],
-*/
-
-    // --- http://hgoebl.github.io/mobile-detect.js/ ---------------------------
-
-/*
-    'mobile-detect' : [
-        'mobile-detect.min.js'
-    ],
-*/
-
-    // --- http://andreknieriem.de/simple-lightbox/ ----------------------------
-
-/*
-    'simplelightbox' : [
-        'simple-lightbox.min.js',
-        'simplelightbox.min.css'
-    ],
-*/
-
-    // --- http://lokeshdhakar.com/projects/lightbox2/ -------------------------
-/*
-    'lightbox2' : [
-        'js/lightbox.min.js',
-        'css/lightbox.min.css',
-        'images/close.png',
-        'images/loading.gif',
-        'images/next.png',
-        'images/prev.png'
-    ],
-*/
+    'lightbox2' : { // ------------------------------------------- Lightbox2 ---
+        install : false,
+        js: [
+            'js/lightbox.min.js'
+        ],
+        css: [
+            'css/lightbox.min.css'
+        ],
+        images: [
+            'images/close.png',
+            'images/close.png',
+            'images/loading.gif',
+            'images/next.png',
+            'images/prev.png'
+        ]
+    }
 };
 
-const fetch = require('node-fetch');
-
-timestamp = function () {
-
-    let d = new Date();
-    let date = [];
-    let time = [];
-
-    date.push( d.getFullYear() );
-    date.push( d.getMonth() );
-    date.push( d.getDate() );
-    time.push( d.getHours() );
-    time.push( d.getMinutes() );
-
-    return date.join('.') + '_' + time.join('') + 'h';
-};
+const fetch  = require('node-fetch');
 
 module.exports = function(grunt) {
     grunt.initConfig({
@@ -93,11 +87,11 @@ module.exports = function(grunt) {
 
         concat : {
             vendorJs : {
-                src: ['temp/vendor/**/*.js'],
+                src: [],
                 dest: 'htdocs/js/vendor.min.js'
             },
             vendorCss : {
-                src: ['temp/vendor/**/*.css'],
+                src: [],
                 dest: 'htdocs/css/vendor.min.css'
             }
         },
@@ -109,13 +103,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     flatten: true,
-                    src: [
-                        'temp/vendor/**/*.otf',
-                        'temp/vendor/**/*.eot',
-                        'temp/vendor/**/*-webfont.svg', // special for fontawesome
-                        'temp/vendor/**/*.ttf',
-                        'temp/vendor/**/*.woff',
-                        'temp/vendor/**/*.woff2'],
+                    src: [],
                     dest: 'htdocs/fonts/'
                 }],
             },
@@ -124,10 +112,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     flatten: true,
-                    src: [
-                        'temp/vendor/**/*.jpg',
-                        'temp/vendor/**/*.png',
-                        'temp/vendor/**/*.gif'],
+                    src: [],
                     dest: 'htdocs/images/'
                 }],
             },
@@ -139,8 +124,7 @@ module.exports = function(grunt) {
             css: {
                 options: {
                     style : "expanded",
-                    noCache: true,
-                    cacheLocation : '.cache/'
+                    noCache: true
                 },
                 files: {
                     'htdocs/css/style.css': 'src/sass/style.scss'
@@ -161,7 +145,6 @@ module.exports = function(grunt) {
         clean: {
             tempFiles: ['temp'],
         },
-
 
         // --- Watcher ---------------------------------------------------------
 
@@ -195,22 +178,86 @@ module.exports = function(grunt) {
             libs    = grunt.config.get().cdnjs.libs,
             apiUrl  = 'https://api.cdnjs.com/libraries/',
             baseUrl = 'https://cdnjs.cloudflare.com/ajax/libs/',
-            curlDir = {};
+            curlDir = {},
+            purge   = {
+                images : [],
+                fonts : []
+            },
+            files   = {
+                js     : [], // to be concatenated
+                css    : [], // "
+                images : [], // to be copied
+                fonts  : []  // "
+            };
 
         for (let lib in libs) {
-            grunt.log.writeln('Fetching ' + lib.cyan);
+
+            if (libs[lib].install) {
+
+                grunt.log.writeln('Fetching: '.green + lib.green);
+
+                for (let type in files) {
+                    if (libs[lib][type]) {
+                        files[type] = files[type].concat(libs[lib][type].map(function (e) {
+                            return "temp/**/" + e.replace(/js\/|css\/|images\/|fonts\//g, '')
+                        }))
+                    }
+                }
+            }
+            else {
+                grunt.log.writeln('Skipping: '.red + lib.red);
+
+                for (let type in purge) {
+                    if (libs[lib][type]) {
+                        purge[type] = purge[type].concat(libs[lib][type].map(function (e) {
+                            return "htdocs/**/" + e.replace(/js\/|css\/|images\/|fonts\//g, '')
+                        }))
+                    }
+                }
+            }
         }
 
-        for (let lib in libs) {
-            let files = (libs[lib].length > 0) ? libs[lib] : false;
+        grunt.config.merge({
+            copy: {
+                vendorFonts: {
+                    files: [{
+                        src: files.fonts
+                    }]
+                },
+                vendorImages: {
+                    files: [{
+                        src: files.images
+                    }]
+                }
+            }
+        });
 
+        grunt.config.merge({
+            concat: {
+                vendorJs: {
+                    src: files.js
+                },
+                vendorCss: {
+                    src: files.css
+                }
+            }
+        });
+
+        grunt.config.merge({
+            clean: {
+                vendorFonts: purge.fonts,
+                vendorImages: purge.images,
+            }
+        });
+
+        for (let lib in libs) {
             fetch(apiUrl + lib + '/')
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(data) {
                     let version = data.assets[0].version,
-                        assets = (files) ? files : data.assets[0].files;
+                        assets  = data.assets[0].files;
 
                     // Prepend the base url to each file
                     let filesFullUri = assets.map(function(file) { return baseUrl + lib + "/" + version + "/" + file });
@@ -228,52 +275,8 @@ module.exports = function(grunt) {
                     grunt.log.writeln('error:' + error.cyan);
                 });
         }
-
     });
 
-    grunt.registerMultiTask('cdnjs', 'Fetch JSON from CDN', function() {
-
-        let done    = this.async(),
-            libs    = grunt.config.get().cdnjs.libs,
-            apiUrl  = 'https://api.cdnjs.com/libraries/',
-            baseUrl = 'https://cdnjs.cloudflare.com/ajax/libs/',
-            curlDir = {};
-
-        for (let lib in libs) {
-            grunt.log.writeln('Fetching ' + lib.cyan);
-        }
-
-        for (let lib in libs) {
-            let files = (libs[lib].length > 0) ? libs[lib] : false;
-
-            fetch(apiUrl + lib + '/')
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(data) {
-                    let version = data.assets[0].version,
-                        assets = (files) ? files : data.assets[0].files;
-
-                    // Prepend the base url to each file
-                    let filesFullUri = assets.map(function(file) { return baseUrl + lib + "/" + version + "/" + file });
-
-                    // push to curlDir
-                    curlDir['temp/vendor/' + lib] = filesFullUri;
-
-                }).then(function(){
-                if (Object.keys(curlDir).length === Object.keys(libs).length) {
-                    grunt.config.set('curl-dir', curlDir);
-                    done(true); // async is done
-                }
-            })
-                .catch(function(error){
-                    grunt.log.writeln('error:' + error.cyan);
-                });
-        }
-
-    });
-
-    grunt.registerTask("init", ['cdnjs', 'curl-dir', 'copy', 'concat', 'clean:tempFiles']);
-    grunt.registerTask("dev", ['coffee', 'sass', 'watch']);
-
+    grunt.registerTask("init", ['cdnjs', 'curl-dir', 'copy', 'concat', 'clean']);
+    grunt.registerTask("dev",  ['coffee', 'sass', 'watch']);
 };
