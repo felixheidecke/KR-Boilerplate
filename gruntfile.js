637b177be38c00811372216ed98d5c4564e85547
Felix Heidecke <felix@heidecke.me>
2017-09-15 17:15:00 +0200

    [Enhancement] Outsource config to a separate file [Enhancement] Do not fetch unwanted libraries

--- gruntfile.js
+++ gruntfile.js
@@ -1,87 +1,4 @@
-let vendor = {
-
-    'jquery' : { // ------------------------------------------------- jQuery ---
-        install : true,
-        js : [
-            'jquery.min.js'
-        ]
-    },
-
-    'pure' : { // ------------------------------------------------- pure CSS ---
-        install : true,
-        css: [
-            'base-min.css',
-            'grids-min.css',
-            'grids-responsive-min.css'
-        ]
-    },
-
-    'font-awesome' : { // ------------------------------------- Font Awesome ---
-        install : true,
-        css: [
-            'css/font-awesome.min.css'
-        ],
-        fonts: [
-            'fonts/fontawesome-webfont.eot',
-            'fonts/fontawesome-webfont.svg',
-            'fonts/fontawesome-webfont.ttf',
-            'fonts/fontawesome-webfont.woff',
-            'fonts/fontawesome-webfont.woff2'
-        ]
-    },
-
-   'vue' : { // ----------------------------------------------------- Vue.js ---
-        install : false,
-        js : [
-           'vue.min.js'
-        ]
-   },
-
-    'mobile-detect' : { // ----------------------------------- Mobile Detect ---
-        install: false,
-        js: [
-            'mobile-detect.min.js'
-        ]
-    },
-
-    'simplelightbox' : { // -------------------------------- Simple lightbox ---
-        install: false,
-        js: [
-            'simple-lightbox.min.js'
-        ],
-        css: [
-            'simplelightbox.min.css'
-        ]
-    },
-
-    'lightbox2' : { // ------------------------------------------- Lightbox2 ---
-        install : false,
-        js: [
-            'js/lightbox.min.js'
-        ],
-        css: [
-            'css/lightbox.min.css'
-        ],
-        images: [
-            'images/close.png',
-            'images/close.png',
-            'images/loading.gif',
-            'images/next.png',
-            'images/prev.png'
-        ]
-    },
-
-    'Swiper' : { // ------------------------------------- iDangero.us Swiper ---
-        install : false,
-        js: [
-            'js/swiper.min.js'
-        ],
-        css: [
-            'css/swiper.min.css'
-        ]
-    }
-};
-
+const config = require('./config.json');
 const fetch  = require('node-fetch');
 const moment = require('moment');
 
@@ -89,7 +6,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         cdnjs : {
-            libs : vendor
+            libs : config.vendor
         },
 
         // --- Concat Vendor files ---------------------------------------------
@@ -202,6 +119,8 @@ module.exports = function(grunt) {
             libs    = grunt.config.get().cdnjs.libs,
             apiUrl  = 'https://api.cdnjs.com/libraries/',
             baseUrl = 'https://cdnjs.cloudflare.com/ajax/libs/',
+            exclude = [];
+            libCount = 0;
             curlDir = {},
             purge   = {
                 images : [],
@@ -230,7 +149,7 @@ module.exports = function(grunt) {
             }
             else {
                 grunt.log.writeln('Skipping: '.red + lib.red);
-
+                exclude.push(lib);
                 for (let type in purge) {
                     if (libs[lib][type]) {
                         purge[type] = purge[type].concat(libs[lib][type].map(function (e) {
@@ -275,34 +194,36 @@ module.exports = function(grunt) {
         });
 
         for (let lib in libs) {
-            fetch(apiUrl + lib + '/')
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(data) {
-                    let version = data.assets[0].version,
-                        assets  = data.assets[0].files;
+            if (libs[lib].install) {
+                fetch(apiUrl + lib + '/')
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        let version = data.assets[0].version,
+                            assets  = data.assets[0].files;
 
-                    // Prepend the base url to each file
-                    let filesFullUri = assets.map(function(file) { return baseUrl + lib + "/" + version + "/" + file });
+                        // Prepend the base url to each file
+                        let filesFullUri = assets.map(function(file) { return baseUrl + lib + "/" + version + "/" + file });
 
-                    // push to curlDir
-                    curlDir['temp/vendor/' + lib] = filesFullUri;
+                        // push to curlDir
+                        curlDir['temp/vendor/' + lib] = filesFullUri;
 
-                }).then(function(){
-                    if (Object.keys(curlDir).length === Object.keys(libs).length) {
-                        grunt.config.set('curl-dir', curlDir);
-                        done(true); // async is done
-                    }
-                })
-                .catch(function(error){
-                    grunt.log.writeln('error:' + error.cyan);
-                });
+                    }).then(function(){
+                        if (Object.keys(curlDir).length === Object.keys(libs).length - exclude.length) {
+                            grunt.config.set('curl-dir', curlDir);
+                            done(true); // async is done
+                        }
+                    })
+                    .catch(function(error){
+                        grunt.log.writeln('error:' + error.cyan);
+                    });
+            }
         }
     });
 
-    grunt.registerTask("setup", ['cdnjs', 'curl-dir', 'copy', 'concat', 'clean']);
-    grunt.registerTask("watcher",  ['coffee', 'sass', 'watch']);
-    grunt.registerTask("build",  ['coffee', 'sass']);
+    grunt.registerTask("setup",   ['cdnjs', 'curl-dir', 'copy', 'concat', 'clean']);
+    grunt.registerTask("watcher", ['coffee', 'sass', 'watch']);
+    grunt.registerTask("build",   ['coffee', 'sass']);
     grunt.registerTask("backup",  ['zip:backup']);
 };
