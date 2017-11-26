f765e8d9cbfa21eb13cbf7b37a3a68b6015e8851
Felix Heidecke <felix@heidecke.me>
2017-11-26 20:39:14 +0100

    Add gruntfile to backup

--- gruntfile.js
+++ gruntfile.js
@@ -210,6 +210,7 @@ module.exports = function(grunt) {
                     'src/**/*',
                     'gruntfile.js',
                     'package.json',
+                    'header.js',
                     'config.json',
                     'KR-Boilerplate.bat' ],
                 dest: 'backup/project-' + moment().format('DD-MM-YYYY_HH[h]-mm[m]-ss[s]') + '.zip'
