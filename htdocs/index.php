fc1b2c10ed0eb41beac5a94f4ee0cca365ee732a
Felix Heidecke <felix@heidecke.me>
2018-11-13 18:42:37 +0100

    Remove Demo code

--- htdocs/index.php
+++ htdocs/index.php
@@ -15,49 +15,10 @@
     <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body hidden>
-    <main>
-        <p>
-            <a href="link.html">Link</a><br>
-            <a href="http://extern/link.html">Link Extern</a><br>
-            <a href="link.pdf">PDF File</a>
-        </p>
-        <p>
-            <span data-mailto>mail[at]wurst[dot]de</span>
-        </p>
-        <ul class="akkordeon">
-            <li class="card">
-                <h3 class="tab">
-                    This is a tab
-                </h3>
-                <div class="content">
-                    <p>
-                        Hier ist Inhalt
-                    </p>
-                </div>
-            </li>
-            <li class="card">
-                <h3 class="tab">
-                    This is second tab
-                </h3>
-                <div class="content">
-                    <p>
-                        Hier ist auch hier
-                    </p>
-                </div>
-            </li>
-        </ul>
-        <div class="foo" data-background-image="bild.gif">
-            <p>
-                Hier ein Hintergrund
-            </p>
-        </div>
 
-        <div class="swiper"></div>
-    </main>
-
-<!--
-    Webseiten Inhalte
--->
+    <!--
+        Webseiten Inhalte
+    -->
 
     <script data-main="js/default.js" src="https://cdn.klickrhein.de/libs/require.js/2.3.6/require.min.js"></script>
 </body>
