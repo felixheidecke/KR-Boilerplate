dc6fc0ee81119c588f71a954ac26fcd6021f6ef6
Felix Heidecke <felix@heidecke.me>
2019-04-25 17:57:58 +0200

    mobile detect is needed for krNav

--- src/script/default.js
+++ src/script/default.js
@@ -14,7 +14,7 @@ requirejs(["jquery"], function($) {
 	require('scripts/js/external.js');			// External
 	require('scripts/js/mailto.js');			// Mailto
 	require('scripts/js/toplink.js');			// Toplink
-//	require('scripts/js/mobile-detect.js');		// Mobile detect
+	require('scripts/js/mobile-detect.js');		// Mobile detect
 //	require('scripts/js/background.js');		// Background
 //	require('scripts/js/shop.js'); 				// KR-Shop
 
