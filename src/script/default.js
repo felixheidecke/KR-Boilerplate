2ce57cf67790744b501d5a3d9bd343f4f743ff5d
Felix Heidecke <felix@heidecke.me>
2019-12-13 08:15:30 +0100

    Add v-cloak to hide unloaded tempaltes

--- src/script/default.js
+++ src/script/default.js
@@ -6,26 +6,24 @@ import config from 'config.js';
 requirejs.config(config);
 
 
-/* --------------------------- *
- * Standard Skripte aktivieren *
- * --------------------------- */
+/* ------------------ *
+ * Skripte aktivieren *
+ * ------------------ */
 
 require('scripts/js/external.js');
 require('scripts/js/mailto.js');
 require('scripts/js/toplink.js');
 require('scripts/js/mobile-detect.js');
-// require('scripts/js/xioni-news.js');
+//require('scripts/js/xioni-news.js');
 // require('scripts/js/shop.js');
 // require('scripts/js/akkordeon.js');
 
+/* --------------------------------- *
+ * Skript Instanzen starten wo nötig *
+ * Bspw.: krNav, krAkkordeon, ect.   *
+ * --------------------------------- */
 
-/* ------------------------------- *
- * Skripte instanzen starten       *
- * dort wo nötig:				   *
- * Bspw.: krNav, krAkkordeon, ect. *
- * ------------------------------- */
-
-// require('scripts/js/nav.js');
-// requirejs(['jquery'], $ => {
-// 	$('.kr-nav-main').krNav();
-// });
+require('scripts/js/nav.js');
+requirejs(['jquery'], $ => {
+	$('.kr-nav-main').krNav();
+});
