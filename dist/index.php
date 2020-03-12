259a6d5c77ca6898f493007ff56cdc6a45157244
Felix Heidecke <felixheidecke@users.noreply.github.com>
2020-03-12 16:04:19 +0100

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -2,15 +2,13 @@
 <html lang="de">
 <!--  klickrhein, Frank Förster, Oestrich-Winkel, Tel. 06723-3410, www.klickrhein.de  -->
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="" />
 	<title></title>
 	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="images/favicon.png" />
 	<link rel="apple-touch-icon" type="image/png" href="images/favicon.png" />
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 
@@ -18,6 +16,6 @@
 	Webseiten Inhalte
 -->
 
-<script src="js/default.js"></script>
+<script src="js/bundle.js"></script>
 </body>
 </html>
