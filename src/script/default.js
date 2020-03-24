8cd8395763ca18e765fd95248704cb07b9569e42
Felix Heidecke <felixheidecke@users.noreply.github.com>
2020-03-24 13:33:34 +0100

    Update default.js

--- src/script/default.js
+++ src/script/default.js
@@ -11,20 +11,20 @@ require('style')
 //  * Skripte aktivieren *
 //  * ------------------ */
 
-require('scripts/js/external.js');
-require('scripts/js/mailto.js');
-require('scripts/js/toplink.js');
-require('scripts/js/mobile-detect.js');
-// require('scripts/js/xioni-news.js');
-// require('scripts/js/shop.js');
-// require('scripts/js/akkordeon.js');
+require('scripts/external.js');
+require('scripts/mailto.js');
+require('scripts/toplink.js');
+require('scripts/mobile-detect.js');
+// require('scripts/xioni-news.js');
+// require('scripts/shop.js');
+// require('scripts/akkordeon.js');
 
 // /* --------------------------------- *
 //  * Skript Instanzen starten wo nötig *
 //  * Bspw.: krNav, krAkkordeon, ect.   *
 //  * --------------------------------- */
 
-require('scripts/js/nav.js');
+require('scripts/nav.js');
 cdn.require(['jquery']).then( () => {
   $('.kr-nav-main').krNav()
 })
