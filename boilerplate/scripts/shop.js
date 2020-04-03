c33b8eb1837cff4491d28ee801e4fafc397a8772
Felix Heidecke <felixheidecke@users.noreply.github.com>
2020-04-03 17:05:22 +0200

    Update shop.js

--- boilerplate/scripts/shop.js
+++ boilerplate/scripts/shop.js
@@ -1,33 +1,33 @@
 cdn.require(['jquery']).then( () => {
 
-	let $form = $(this).find('form.cart');
-	let $action = $(this).find('.js-form-action');
-	let $itemCount = $(this).find('.js-form-item-count');
-	let $itemRemove = $(this).find('.js-form-item-remove');
-	let $submit = $(this).find('.js-form-submit');
-	let $update = $(this).find('.js-form-update');
+  const $form       = $('.kr-shop').find('form.cart');
+  const $action     = $('.kr-shop').find('.js-form-action');
+  const $itemCount  = $('.kr-shop').find('.js-form-item-count');
+  const $itemRemove = $('.kr-shop').find('.js-form-item-remove');
+  const $submit     = $('.kr-shop').find('.js-form-submit');
+  const $update     = $('.kr-shop').find('.js-form-update');
 
-	const run = function(action) {
-		$action.val(action);
-		$form.submit();
-	};
+  const run = function(action) {
+    $action.val(action);
+    $form.submit();
+  };
 
-	$update.on('click', function(e) {
-		e.preventDefault();
-		return run('update');
-	});
+  $update.on('click', function(e) {
+    e.preventDefault();
+    return run('update');
+  });
 
-	$itemCount.on('change', function(e) {
-		e.preventDefault();
-		return run('update');
-	});
+  $itemCount.on('change', function(e) {
+    e.preventDefault();
+    return run('update');
+  });
 
-	$submit.on('click', function(e) {
-		e.preventDefault();
-		return run('payment');
-	});
+  $submit.on('click', function(e) {
+    e.preventDefault();
+    return run('payment');
+  });
 
-	return $itemRemove.on('click', function(e) {
-		return e.preventDefault();
-	});
+  return $itemRemove.on('click', function(e) {
+    return e.preventDefault();
+  });
 })
