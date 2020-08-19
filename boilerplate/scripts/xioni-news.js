98e7d185994e6a997f983c769d6493c83fc22695
Felix Heidecke <felix@heidecke.me>
2020-08-19 22:07:58 +0200

    urlify urls in xioni news string

--- boilerplate/scripts/xioni-news.js
+++ boilerplate/scripts/xioni-news.js
@@ -6,12 +6,12 @@ cdn.require(['jquery', 'vue', 'lodash', 'textile', `moment.js/${dependencies.mom
 
     $('body').find("[data-xioni^='news.']:not(.xioni-prevent-default)").each(function() {
         var dataParams, dataXioni, unique;
-        unique = 'xid-' + Math.floor(Math.random() * Math.pow(83, 6));
-        dataXioni = $(this).data('xioni').split('.');
-        dataParams = false;
+        unique = 'xid-' + Math.floor(Math.random() * Math.pow(83, 6))
+        dataXioni = $(this).data('xioni').split('.')
+        dataParams = false
         if ($(this).data('params')) {
-            dataParams = $(this).data('params').replace(/\'/g, '"');
-            dataParams = JSON.parse(dataParams);
+            dataParams = $(this).data('params').replace(/\'/g, '"')
+            dataParams = JSON.parse(dataParams)
         }
         $(this).attr('id', unique);
         $(this).removeAttr('data-xioni');
@@ -23,38 +23,58 @@ cdn.require(['jquery', 'vue', 'lodash', 'textile', `moment.js/${dependencies.mom
                 details: false,
                 params: dataParams
             },
+
             computed: {
                 url () {
-                    return 'https://api.klickrhein.de/' + dataXioni[0] + '.php?id=' + dataXioni[1];
+                    return 'https://api.klickrhein.de/' + dataXioni[0] + '.php?id=' + dataXioni[1]
                 }
             },
+
             methods: {
                 dateFormat (timestamp, format) {
                     if (format == null) {
-                        format = 'LL';
+                        format = 'LL'
                     }
-                    return moment.unix(timestamp).format(format);
+                    return moment.unix(timestamp).format(format)
                 },
+
                 textile (string) {
-                    return textile(string.trim());
+                  let markup = this.urlify(string.trim())
+                      markup = textile(markup)
+                  return markup
                 },
+
+                urlify (text) {
+                  let urlRegex  = /(https?:\/\/[^\s]+)/g
+                  let trimRegex = /(https?:\/\/)/
+
+                  return text.replace(urlRegex, url => {
+                    let trimedSheme = url.replace(trimRegex, '')
+                    return `<a href="${url}">${trimedSheme}</a>`
+                  })
+                },
+
                 dataPath (item) {
                     if (item) {
-                        return this.urlBase + item;
+                        return this.urlBase + item
                     }
                 },
+
                 goTo (id) {
-                    this.details = _.filter(this.items, ['id', id])[0];
-                    window.location = "#" + id;
-                    this.scrollTop();
+                    this.details = _.filter(this.items, ['id', id])[0]
+                    window.location = "#" + id
+                    this.scrollTop()
                 },
+
                 goBack () {
                     this.details = false;
                     window.location = '#';
                 },
+
                 trimSheme (url) {
                     return url.replace(/.*:\/\//, '')
                 },
+
                 scrollTop () {
                     $('body,html').animate({
                         scrollTop: $("#" + unique).offset().top
@@ -66,9 +86,10 @@ cdn.require(['jquery', 'vue', 'lodash', 'textile', `moment.js/${dependencies.mom
                 $.getJSON(this.url, function(data) {
                     ref.items = data;
                     if (window.location.hash) {
-                        ref.goTo(window.location.hash.replace(/#/, ''));
+                        ref.goTo(window.location.hash.replace(/#/, ''))
                     }
-                });
+                })
+
                 $(window).bind('popstate', function() {
                     if (window.location.hash.length === 0) {
                         ref.details = false;
