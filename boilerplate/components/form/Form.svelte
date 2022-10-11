23aa9065c6e67b4d40e77806ff9219fdc4564bce
Felix Heidecke <felix@heidecke.me>
2022-10-11 13:50:15 +0200

    Don't scroll to $$slot.done if it's empty

--- boilerplate/components/form/Form.svelte
+++ boilerplate/components/form/Form.svelte
@@ -24,7 +24,7 @@
   // --- Methods ----------------------
 
   const scrollToDoneText = () => {
-    if (!isDoneEl) return
+    if (!isDoneEl || !$$slots.done) return
 
     isDoneEl.scrollIntoView({
       behavior: `smooth`
