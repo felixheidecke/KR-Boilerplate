5dcdc5422ba8be8ffdbd7bc1133e646277923d33
Felix Heidecke <felix@heidecke.me>
2023-04-25 19:42:41 +0200

    Add "Icons in a List" feature

--- src/lib/boilerplate/actions/icon.ts
+++ src/lib/boilerplate/actions/icon.ts
@@ -1,13 +1,17 @@
 const createIconElement = (icon: string): HTMLElement => {
-	const $el = document.createElement('i')
-	$el.setAttribute('class', `Icon ${icon} $mr-1/2`)
+	const $span = document.createElement('span')
+	$span.classList.add('fa-li')
+	$span.innerHTML = `<i class="${icon}"></i>`
 
-	return $el
+	return $span
 }
 
 export default (node: HTMLElement, icon = 'fas fa-circle') => {
+	node.classList.add('fa-ul')
+
 	node.querySelectorAll(':scope > li').forEach(li => {
 		const $icon = createIconElement(icon)
-		li.prepend($icon.cloneNode())
+
+		li.prepend($icon)
 	})
 }
