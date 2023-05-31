9151de9fedc4ed11f62971fc8451257485b52799
Felix Heidecke <felix@heidecke.me>
2023-05-31 11:33:29 +0200

    Bump deps

--- src/routes/(auxiliary)/info.json/+server.ts
+++ src/routes/(auxiliary)/info.json/+server.ts
@@ -7,13 +7,7 @@ export const GET = function () {
 	const data = {
 		name: pkg.name,
 		version: pkg.version,
-		build: new Date().toLocaleString(LOCALE, {
-			month: '2-digit',
-			day: '2-digit',
-			year: 'numeric',
-			hour: '2-digit',
-			minute: '2-digit'
-		})
+		build: new Date().toLocaleString(LOCALE)
 	}
 	const response = new Response(JSON.stringify(data))
 
