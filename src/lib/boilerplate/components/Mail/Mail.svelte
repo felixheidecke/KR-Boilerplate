ea1e478ee75726a2a9699d6e6fbc3ac086f63f31
Felix Heidecke <felix@heidecke.me>
2024-06-28 11:15:21 +0200

    4.16.2

--- src/lib/boilerplate/components/Mail/Mail.svelte
+++ src/lib/boilerplate/components/Mail/Mail.svelte
@@ -10,18 +10,38 @@
 	// --- [ Props ] ---------------------------------------------------------------------------------
 
 	export let to: string
-	export let icon: string | undefined = 'fas fa-envelope'
+	export let icon: string | null | false = 'fas fa-envelope'
+	export let subject: string | undefined = undefined
+	export let body: string | undefined = undefined
 
 	// --- [ Logic ] ---------------------------------------------------------------------------------
 
 	const obfuscatedEmail = to.replaceAll('@', '&#64;').replaceAll('.de', '&#8228;&#100;&#101;')
 	const baseName = $$props['ex-class'] || 'Mail'
 	const className = classnames(baseName, $$props.class)
+	const showIcon = typeof icon === 'string' && icon !== 'false'
+
+	function makeHref() {
+		const link = new URL('mailto:' + to)
+
+		if (subject) {
+			link.searchParams.set('subject', subject)
+		}
+
+		if (body) {
+			link.searchParams.set('body', body)
+		}
+
+		return link.toString()
+	}
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<span on:click|preventDefault={() => (location.href = 'mailto:' + to)} class={className}>
-	{#if icon}
+<span
+	on:click|preventDefault={() => (location.href = makeHref())}
+	{...$$restProps}
+	class={className}>
+	{#if showIcon && icon}
 		<Icon ex-class={baseName + '__icon'} name={icon} />
 	{/if}
 	<span class={baseName + '__address'}>
