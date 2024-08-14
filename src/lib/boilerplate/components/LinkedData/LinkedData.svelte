dadd05c8b5563abae393629030d22d027ce055c5
Felix Heidecke <felix@heidecke.me>
2024-08-14 20:15:26 +0200

    5.0.5

---
+++ src/lib/boilerplate/components/LinkedData/LinkedData.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let data: Record<string, any>
+
+	const stringifiedData = JSON.stringify({
+		'@context': 'https://schema.org',
+		...data
+	})
+</script>
+
+<svelte:element this="script" type="application/ld+json">{stringifiedData}</svelte:element>
